7b39ab0ac7dc4d1e432802ecc16709fdbcf853d0
flames-09 <flamesrise09@gmail.com>
2026-01-04 22:20:08 -0800

    Create carddata.jsx

---
+++ carddata.jsx
@@ -0,0 +1,83 @@
+export const allCards = [
+  // Arena 1
+  { name: "Arrows", rarity: "common", elixir: 3, type: "spell", arena: 1 },
+  { name: "Minions", rarity: "common", elixir: 3, type: "troop", arena: 1 },
+  { name: "Archers", rarity: "common", elixir: 3, type: "troop", arena: 1 },
+  { name: "Knight", rarity: "common", elixir: 3, type: "troop", arena: 1 },
+  { name: "Fireball", rarity: "rare", elixir: 4, type: "spell", arena: 1 },
+  { name: "Mini P.E.K.K.A", rarity: "rare", elixir: 4, type: "troop", arena: 1 },
+  { name: "Musketeer", rarity: "rare", elixir: 4, type: "troop", arena: 1 },
+  { name: "Giant", rarity: "rare", elixir: 5, type: "troop", arena: 1 },
+  { name: "Spear Goblins", rarity: "common", elixir: 2, type: "troop", arena: 1 },
+  { name: "Goblins", rarity: "common", elixir: 2, type: "troop", arena: 1 },
+  { name: "Goblin Cage", rarity: "rare", elixir: 4, type: "building", arena: 1 },
+  { name: "Goblin Hut", rarity: "rare", elixir: 5, type: "building", arena: 1 },
+
+  // Arena 2
+  { name: "Bomber", rarity: "common", elixir: 2, type: "troop", arena: 2 },
+  { name: "Skeletons", rarity: "common", elixir: 1, type: "troop", arena: 2 },
+  { name: "Tombstone", rarity: "rare", elixir: 3, type: "building", arena: 2 },
+  { name: "Valkyrie", rarity: "rare", elixir: 4, type: "troop", arena: 2 },
+
+  // Arena 3
+  { name: "Cannon", rarity: "common", elixir: 3, type: "building", arena: 3 },
+  { name: "Barbarians", rarity: "common", elixir: 5, type: "troop", arena: 3 },
+  { name: "Mega Minion", rarity: "rare", elixir: 3, type: "troop", arena: 3 },
+  { name: "Battle Ram", rarity: "rare", elixir: 4, type: "troop", arena: 3 },
+
+  // Arena 4
+  { name: "Electro Spirit", rarity: "common", elixir: 1, type: "troop", arena: 4 },
+  { name: "Skeleton Dragons", rarity: "common", elixir: 4, type: "troop", arena: 4 },
+  { name: "Fire Spirit", rarity: "common", elixir: 1, type: "troop", arena: 4 },
+  { name: "Bomb Tower", rarity: "rare", elixir: 4, type: "building", arena: 4 },
+  { name: "Inferno Tower", rarity: "rare", elixir: 5, type: "building", arena: 4 },
+  { name: "Wizard", rarity: "rare", elixir: 5, type: "troop", arena: 4 },
+
+  // Arena 5
+  { name: "Zap", rarity: "common", elixir: 2, type: "spell", arena: 5 },
+  { name: "Mortar", rarity: "common", elixir: 4, type: "building", arena: 5 },
+  { name: "Bats", rarity: "common", elixir: 2, type: "troop", arena: 5 },
+  { name: "Rocket", rarity: "rare", elixir: 6, type: "spell", arena: 5 },
+  { name: "Flying Machine", rarity: "rare", elixir: 4, type: "troop", arena: 5 },
+  { name: "Hog Rider", rarity: "rare", elixir: 4, type: "troop", arena: 5 },
+
+  // Arena 6
+  { name: "Goblin Barrel", rarity: "epic", elixir: 3, type: "spell", arena: 6 },
+  { name: "Guards", rarity: "epic", elixir: 3, type: "troop", arena: 6 },
+  { name: "Skeleton Army", rarity: "epic", elixir: 3, type: "troop", arena: 6 },
+  { name: "Baby Dragon", rarity: "epic", elixir: 4, type: "troop", arena: 6 },
+  { name: "Witch", rarity: "epic", elixir: 5, type: "troop", arena: 6 },
+  { name: "P.E.K.K.A", rarity: "epic", elixir: 7, type: "troop", arena: 6 },
+
+  // Arena 7
+  { name: "Royal Recruits", rarity: "common", elixir: 7, type: "troop", arena: 7 },
+  { name: "Royal Giant", rarity: "common", elixir: 6, type: "troop", arena: 7 },
+  { name: "Royal Hogs", rarity: "rare", elixir: 5, type: "troop", arena: 7 },
+  { name: "Three Musketeers", rarity: "rare", elixir: 9, type: "troop", arena: 7 },
+  { name: "Dark Prince", rarity: "epic", elixir: 4, type: "troop", arena: 7 },
+  { name: "Prince", rarity: "epic", elixir: 5, type: "troop", arena: 7 },
+  { name: "Balloon", rarity: "epic", elixir: 5, type: "troop", arena: 7 },
+
+  // Arena 8
+  { name: "Snowball", rarity: "common", elixir: 2, type: "spell", arena: 8 },
+  { name: "Ice Spirit", rarity: "common", elixir: 1, type: "troop", arena: 8 },
+  { name: "Battle Healer", rarity: "rare", elixir: 4, type: "troop", arena: 8 },
+  { name: "Ice Golem", rarity: "rare", elixir: 2, type: "troop", arena: 8 },
+  { name: "Vines", rarity: "rare", elixir: 2, type: "spell", arena: 8 },
+  { name: "Lightning", rarity: "epic", elixir: 6, type: "spell", arena: 8 },
+  { name: "Freeze", rarity: "epic", elixir: 4, type: "spell", arena: 8 },
+  { name: "Giant Skeleton", rarity: "epic", elixir: 6, type: "troop", arena: 8 },
+
+  // Arena 9
+  { name: "Berserker", rarity: "common", elixir: 2, type: "troop", arena: 9 },
+  { name: "Skeleton Barrel", rarity: "common", elixir: 3, type: "troop", arena: 9 },
+  { name: "Goblin Gang", rarity: "common", elixir: 3, type: "troop", arena: 9 },
+  { name: "Barbarian Hut", rarity: "rare", elixir: 7, type: "building", arena: 9 },
+  { name: "Dart Goblin", rarity: "rare", elixir: 3, type: "troop", arena: 9 },
+  { name: "Barbarian Barrel", rarity: "epic", elixir: 2, type: "spell", arena: 9 },
+  { name: "Poison", rarity: "epic", elixir: 4, type: "spell", arena: 9 },
+  { name: "Rune Giant", rarity: "epic", elixir: 4, type: "troop", arena: 9 },
+  { name: "Goblin Giant", rarity: "epic", elixir: 6, type: "troop", arena: 9 },
+
+  // Arena 10
+ 
